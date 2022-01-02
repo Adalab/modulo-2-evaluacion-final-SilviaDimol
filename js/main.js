2a9f5da79aac09aaef44769c3e945667db2df138
Silvia Dimol <silviadmolina@gmail.com>
2022-01-02 19:23:42 +0100

    continue js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// Constantes
+// CONSTANTES
 // Input, donde meten la búsqueda
 const searchText = document.querySelector(".js_formInput");
 // Botón de buscar
@@ -11,27 +11,28 @@ const resetButton = document.querySelector("js-resetButton");
 const listSeries = document.querySelector(".js-searchResult");
 // Zona de búsqueda, el form
 const requestPanel = document.querySelector(".js-form");
-// Donde se pintan los favoritos
+// Donde se pintan las favoritas
 const listFav = document.querySelector(".js-favListCompleted");
 
 
-// Variables
+// VARIABLES
 let series = [];
 let favorites = [];
 
 
-// Función para la búsqueda
+// AL ARRANCAR LA PÁGINA
 function handleSearch() {
   apiRequest(searchText.value);
 }
 
-// Función para obtener los datos del API, fetch
+// OBTENER LAS SERIES DEL API
 function apiRequest(userSearch) {
-    fetch("//api.jikan.moe/v3/search/anime?q=naruto" + userSearch) //sin http
-      .then(response => response.json()) //nos pasa todo en crudo
+    fetch("//api.jikan.moe/v3/search/anime?q=naruto" + userSearch) 
+      .then(response => response.json()) 
       .then(data => {
           console.log(data);
         const seriesList = data;
+        // 3º GUARDAR LAS SERIES EN UN ARRAY
         series = [];
         for (const serie of seriesList) {
           series.push(serie.show);
@@ -40,29 +41,20 @@ function apiRequest(userSearch) {
       });
     }
 
-// preventDefault para evitar que la página se recargue involuntariamente
-function handleForm(ev) {
-    ev.preventDefault();
-  }
-
-// Pintar las series
-
-// OK plasmar series
+// PINTAR LAS SERIES
 function paintSeries() {
     // Lo que vamos a rellenar
     listSeries.innerHTML = "";
     let html = "";
     let favClass = "";
-    // Que recorra las series
+    // Bucle: que recorra las series del API
     for (const serie of series) {
       const isFav = isFavorite(serie);
-      // Si la serie es favorita...
       if (isFav) {
         favClass = "serie--favorite";
       } else {
         favClass = "";
       }
-      // Si es favorita, que aparezca listada
       html = `<li id=${serie.id} class= "searchResult_elem js-searchResult_elem ${favClass}">`;
       // Si la serie no tiene imagen
       if (null === serie.image) {
@@ -83,9 +75,112 @@ function paintSeries() {
     }
   }
 
-//// Escuchar cada serie
+// ???
+function isFavorite(idSerie) {
+    // el find devolverá undefined si la serie no está en el array de favoritas
+    const favoriteFound = favorites.find((idFavorite) => {
+        // el return dirá si la serie está o no en favoritas
+      return idFavorite.id === idSerie.id;
+    });
+    if (favoriteFound === undefined) {
+      // return dirá false cuando la serie no esté en favoritas
+      return false;
+    } else {
+      // return dirá true cuando la serie sí esté en favoritas
+      return true;
+    }
+  }
+
+// ESCUCHAR EVENTOS EN LAS SERIES
 function listenClickSeries() {
     const seriesCards = document.querySelectorAll(".js-searchResult_elem");
     for (const serieCard of seriesCards)
       serieCard.addEventListener("click", addFavorites);
   }
+
+
+  // ???
+function addFavorites(ev) {
+    // Sacar id de la serie seleccionada
+    const serieSelected = parseInt(ev.currentTarget.id);
+    // Encontrar el id de la serie seleccionada entre las id de favoritas
+    const serieClicked = series.find((idSerie) => {
+      return idSerie.id === serieSelected;
+    });
+    const favAlready = favorites.findIndex((idFavorite) => {
+      return idFavorite.id === serieSelected;
+    });
+  // Agregar serie
+    if (favAlready === -1) {
+      favorites.push(serieClicked);
+    } else {
+      favorites.splice(favAlready, 1);
+    }
+    paintSeries();
+    paintFavorites();
+  }
+
+// PINTAR LAS SERIES FAVORITAS
+function paintFavorites() {
+    // Lo que vamos a rellenar
+    listFav.innerHTML = "";
+    let htmlFav = "";
+   // Bucle: que recorra las series de favoritas
+    for (const serie of favorites) {
+      htmlFav = `<li id=${serie.id} class="fav_section-list js-favSectionList">`;
+      htmlFav += `<button id="${serie.id}" class="js-deleteCross fav_elem-delete ">x</button>`;
+      // Si la serie favorita no tiene imagen
+      if (null === serie.image) {
+        htmlFav += `<img class="fav_elem-img"
+            src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" 
+            title=${serie.name} alt=${serie.name}/>`;
+      } else {
+          // Si la serie favorita tiene imagen
+        htmlFav += `<img class="fav_elem-img"
+            src=${serie.image.medium}
+            title=${serie.name} alt=${serie.name}/>`;
+      }
+      htmlFav += `<h4 class="fav_elem-serieTitle"> ${serie.name}</h4>`;
+      htmlFav += `</li>`;
+      listFav.innerHTML += htmlFav;
+  
+      listenClickedFavorites();
+    }
+  }
+
+  // ESCUCHAR EVENTOS EN LAS SERIES FAVORITAS
+function listenClickedFavorites() {
+    const favCards = document.querySelectorAll(".js-deleteCross");
+    for (const favCard of favCards) favCard.addEventListener("click", deleteFav);
+  }
+  
+  function deleteFav(ev) {
+    const favClicked = parseInt(ev.currentTarget.id);
+    const favSelected = favorites.findIndex((idFav) => idFav.id === favClicked);
+    favorites.splice(favSelected, 1);
+    paintSeries();
+    paintFavorites();
+  }
+  
+  function favHidden() {
+    const favSection = document.querySelector(".js-favArea");
+    if ((favSection.innerHTML = "")) {
+      favSection.classList.add("js-hidden");
+    } else {
+      favSection.classList.remove("js-hidden");
+    }
+  }
+  
+  // // preventDefault para evitar que la página se recargue involuntariamente
+function handleForm(ev) {
+    ev.preventDefault();
+  }
+  
+  // EVENTOS 
+  // Cuando se pulsa el botón de buscar:
+  searchButton.addEventListener("click", handleSearch);
+  // Cuando se pulsa sobre alguna serie:
+  requestPanel.addEventListener("submit", handleForm);
+  //Cuando se pulsa sobre el botón de reset: resetButton.addEventListener("click", handleReset);
+
+  //FALTA LO DEL LOCALSTORAGE!!!: guardar favoritas en el localstorage y borrarlas con el botón de reset.
